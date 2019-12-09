55c1de0729e00dadeb87fa100af5732ccb74a4c8
Aliyus Rudh <diskgiant@gmail.com>
2019-12-08 20:43:35 -0600

    Test the constructor

--- Person.py
+++ Person.py
@@ -1,6 +1,9 @@
 class Person:
   lastIdUsed=100
-  def __init__(self, name):
-    name=name
-    lastIdUsed+=1
-    pid=lastIdUsed
+  def __init__(self, fn, ln):
+    fname=fn
+    lname=ln
+    Person.lastIdUsed+=1
+    pid=Person.lastIdUsed
+
+p=Person("Jim", "Jones")
